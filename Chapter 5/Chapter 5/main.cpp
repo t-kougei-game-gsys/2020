a8f3b44d23676ec9b1c13b0b74318a4b7850f04d
Kuma <zerox0103@gmail.com>
2020-06-13 18:16:06 +0900

    Chapter 5 Init

---
+++ Chapter 5/Chapter 5/main.cpp
@@ -0,0 +1,93 @@
+#include "D3DUtility.h"
+
+#include <iostream>
+
+using namespace D3D;
+
+const unsigned int window_width = 1280;
+const unsigned int window_height = 720;
+
+D3D_DESC _d3dDesc = {};
+
+bool Display (float deltaTime) {
+	int bbIdx = _d3dDesc.SwapChain->GetCurrentBackBufferIndex ();
+
+	auto rtvH = _d3dDesc.RTVHeap->GetCPUDescriptorHandleForHeapStart ();
+	rtvH.ptr += bbIdx * _d3dDesc.Device->GetDescriptorHandleIncrementSize (D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
+
+	_d3dDesc.BarrierDesc.Transition.pResource = _d3dDesc.BackBuffers[bbIdx];
+	_d3dDesc.BarrierDesc.Transition.StateBefore = D3D12_RESOURCE_STATE_PRESENT;
+	_d3dDesc.BarrierDesc.Transition.StateAfter = D3D12_RESOURCE_STATE_RENDER_TARGET;
+	_d3dDesc.CMDList->ResourceBarrier (1, &_d3dDesc.BarrierDesc);
+
+	_d3dDesc.CMDList->OMSetRenderTargets (1, &rtvH, false, nullptr);
+
+	_d3dDesc.CMDList->ClearRenderTargetView (rtvH, BLACK, 0, nullptr);
+
+	_d3dDesc.CMDList->Close ();
+
+	ID3D12CommandList* cmdLists[] = {_d3dDesc.CMDList};
+	_d3dDesc.CMDQueue->ExecuteCommandLists (1, cmdLists);
+
+	_d3dDesc.CMDQueue->Signal (_d3dDesc.Fence, ++_d3dDesc.FenceVal);
+	if (_d3dDesc.Fence->GetCompletedValue () != _d3dDesc.FenceVal) {
+		auto event = CreateEvent (nullptr, false, false, nullptr);
+		_d3dDesc.Fence->SetEventOnCompletion (_d3dDesc.FenceVal, event);
+		WaitForSingleObject (event, INFINITE);
+		CloseHandle (event);
+	}
+
+	_d3dDesc.CMDAllocator->Reset ();
+	_d3dDesc.CMDList->Reset (_d3dDesc.CMDAllocator, nullptr);
+
+	_d3dDesc.SwapChain->Present (1, 0);
+
+	return true;
+}
+
+LRESULT CALLBACK D3D::WndProc (HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+	switch (msg) {
+		case WM_DESTROY:
+			PostQuitMessage (0);
+			break;
+		case WM_KEYDOWN:
+			if (wParam == VK_ESCAPE)
+				DestroyWindow (hwnd);
+	}
+
+	return DefWindowProc (hwnd, msg, wParam, lParam);
+}
+
+void EnableDebugLayer () {
+	ID3D12Debug* debugLayer = 0;
+	if (SUCCEEDED (D3D12GetDebugInterface (IID_PPV_ARGS (&debugLayer)))) {
+		debugLayer->EnableDebugLayer ();
+		debugLayer->Release ();
+	}
+}
+
+bool Setup () {
+	return true;
+}
+
+int main () {
+	EnableDebugLayer ();
+
+	if (!InitD3D (GetModuleHandle (nullptr), window_width, window_height, &_d3dDesc)) {
+		printf_s ("ERROR : InitD3D() - FAILED\n");
+		return -1;
+	}
+
+	if (!Setup ()) {
+		printf_s ("ERROR : Setup() - FAILED\n");
+		return -1;
+	}
+
+	D3D::EnterMsgLoop (Display);
+
+	UnregisterClass (_d3dDesc.WNDClass.lpszClassName, _d3dDesc.WNDClass.hInstance);
+
+	printf_s ("Application end.");
+
+	return 0;
+}
