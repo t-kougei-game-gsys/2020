f2c01b3f357d1c2507811be544e8ff703d4bd3c0
Kuma <zerox0103@gmail.com>
2020-08-12 19:47:55 +0900

    Black And White Photo

--- Chapter 12/Chapter 12/PeraPixel.hlsl
+++ Chapter 12/Chapter 12/PeraPixel.hlsl
@@ -2,5 +2,9 @@
 
 float4 main (Output input) : SV_TARGET {
 	//return float4 (input.uv, 1, 1);
-	return tex.Sample (smp, input.uv);
+	//return tex.Sample (smp, input.uv);
+
+	float4 col = tex.Sample (smp, input.uv);
+	float Y = dot(col.rgb, float3(0.299, 0.587, 0.114));
+	return float4 (Y, Y, Y, 1);
 }
