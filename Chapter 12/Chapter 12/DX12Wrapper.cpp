fb30bc0d40209babe691bde123bae64ab217a19f
Kuma <zerox0103@gmail.com>
2020-08-14 18:20:49 +0900

    fix something

--- Chapter 12/Chapter 12/DX12Wrapper.cpp
+++ Chapter 12/Chapter 12/DX12Wrapper.cpp
@@ -815,7 +815,11 @@ bool DX12Wrapper::CreatePeraResourcesAndView () {
 
 	_dev->CreateRenderTargetView (_peraResource.Get (), &rtvDesc, handle);
 
-	handle.ptr += _dev->GetDescriptorHandleIncrementSize (D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
+	//handle.ptr += _dev->GetDescriptorHandleIncrementSize (D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
+	handle.ptr += _dev->GetDescriptorHandleIncrementSize (D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV);
+
+	printf ("%i\n", _dev->GetDescriptorHandleIncrementSize (D3D12_DESCRIPTOR_HEAP_TYPE_RTV));
+	printf ("%i\n", _dev->GetDescriptorHandleIncrementSize (D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV));
 
 	_dev->CreateRenderTargetView (
 		_peraResource2.Get (),
